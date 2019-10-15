294be1de8416ffa9fb260de925338b9a8b00076f
Hemant Vaniya <hemantvania@gmail.com>
2019-10-15 18:44:01 +0530

    Initial commit:

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Himatlal.Vaniya
+ * Date: 10/14/2019
+ * Time: 3:38 PM
+ */
+
+namespace   App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response('Homepage is found');
+    }
+
+    /**
+     * @Route("/users")
+     */
+    public function users()
+    {
+        $users = ['Hemant', 'Rakesh', 'Kiran', 'Mohan'];
+        return new Response(json_encode($users));
+    }
+
+    /**
+     * @Route("/user/{id}")
+     * @param $id
+     * @return Response
+     */
+    public function user($id)
+    {
+        return new Response("THis is user id: ".$id);
+    }
+}
